817a6ca56b7480c727018c3a3b5de9eff4faeb8e
gicudobrin <mihaiclaudiu87@yahoo.com>
2017-11-02 18:42:17 +0200

    How to create a function - example

--- 1. Python basics/1. Python for beginners/introduction.py
+++ 1. Python basics/1. Python for beginners/introduction.py
@@ -3,3 +3,8 @@ print "Hello World!"
 # Create a variable
 
 launch = "turkey sandwich"
+
+# Create a function (it can accept arguments and parameters)
+
+def answer_the_phone()
+	print("Hi, this is Americanu. Who is this")
