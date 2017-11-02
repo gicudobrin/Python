4c3be57a3edbe39178ff5c4569bc505625e9f075
gicudobrin <mihaiclaudiu87@yahoo.com>
2017-11-02 21:45:09 +0200

    update folder data types

---
+++ 1. Python basics/2. Python Data Types/numbers.py
@@ -0,0 +1,30 @@
+# whole numbers = int (integers)
+print(5 + 5)
+print(5 * 10)
+
+# Division always gives a float.
+
+print(8/2)
+
+# Division by 0 ZeroDivisionError
+
+#10/0
+
+print(5.8 + 2.5)
+print(1.2 + 0.3)
+exponent = 0.1 + 0.1 + 0.1 - 0.3
+print(int(5.2))
+print(exponent)
+print(round(exponent))
+print(int(5.9))
+print(round(5.9))
+
+age = 34
+age += 7
+print(age)
+
+# Age calculation
+years = 30
+days = 30 * 365
+weeks = days / 7
+print(weeks)
---
+++ 1. Python basics/2. Python Data Types/variables.py
@@ -0,0 +1,18 @@
+# Creating a variable is always done the same way:
+
+# variable_name = "variable value"
+
+# And you can always delete a variable using:
+
+# del variable_name
+
+
+# You can't use hyphens, spaces or any alphanumeric characters.
+# You can't also start a variable name with a number.
+
+# Example:
+favorite_fruit = "apple"
+favorite_number = 42
+print(favorite_fruit)
+
+print(favorite_number/42)
