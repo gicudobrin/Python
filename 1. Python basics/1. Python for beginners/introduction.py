b89e5cc211a7a18676b0d77436aee9e2b44104b4
gicudobrin <mihaiclaudiu87@yahoo.com>
2017-11-02 19:38:08 +0200

    updated introduction file

--- 1. Python basics/1. Python for beginners/introduction.py
+++ 1. Python basics/1. Python for beginners/introduction.py
@@ -7,4 +7,33 @@ launch = "turkey sandwich"
 # Create a function (it can accept arguments and parameters)
 
 def answer_the_phone()
-	print("Hi, this is Americanu. Who is this")
+	print("Hi, this is Jane. Who is this?")
+
+# Triggering a function is known as calling it.
+	
+	answer_the_phone()
+
+# All of our values are actually objects.
+
+# Everything is an object. So everything has attributes and methods.
+
+# Blocks are indented one extra step. Them also start with a colon.
+
+# Syntax is what we call the rules for writing in a programming language.
+
+# The Python Shell - REPL = read - evaluate - print loop
+
+#	type python to be in the shell.
+#   type exit() to quit it.
+
+# We can use help to find out more about a function.
+  
+# Example: help(print)
+
+# You can hint q to exit from help menu.
+
+# Python scripts tell Python what to do and say.
+
+# We can run a script: python filename.py
+
+# Errors messages: NameError(5 + x), TypeError(5 + "a"), SyntaxError(EOL = end of line)
