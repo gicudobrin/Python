3f2b50530013acaf3f9accc82dd3bc0b7b34d663
gicudobrin <mihaiclaudiu87@yahoo.com>
2017-11-02 22:39:55 +0200

    finished strings

--- 1. Python basics/2. Python Data Types/3. strings.py
+++ 1. Python basics/2. Python Data Types/3. strings.py
@@ -4,3 +4,42 @@
 # We can use single or double quotes, but they have to be the same on both sides.
 
 print('He\'s right')
+print("""He's right""")
+print('''He's right''')
+
+long_string = """Here's a new line!
+
+
+It's right there!"""
+print(long_string)
+print(str(5))
+
+# Strings concatenation
+
+print("Hello" + " " + "there!")
+
+name = "Claudiu"
+name += " "
+name += "Craciunoiu"
+
+print(name)
+
+var = "a"
+var1 = str(5)
+print(var + var1)
+
+# You can't subtract or divide strings
+# You can multiply them
+
+print("=" * 10)
+
+status_message = "Hey, we have 8 people using the site right now."
+status_message = "Hey we have {} people using the site right now."
+print(status_message.format(7)) 
+print("Hey we have {} {} using the site right now".format(5, "dogs"))
+
+print("=" * 10)
+
+nume = "Americanu"
+subject = "Treehouse loves" + " " + nume
+print(subject)
