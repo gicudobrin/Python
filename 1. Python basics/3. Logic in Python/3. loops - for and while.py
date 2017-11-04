7db00d0acfb77aac72ba8742b0ca115248a74766
gicudobrin <mihaiclaudiu87@yahoo.com>
2017-11-04 02:07:14 +0200

    upsate file

---
+++ 1. Python basics/3. Logic in Python/3. loops - for and while.py
@@ -0,0 +1,44 @@
+
+
+for name in ['Bob', 'Emily', 'Jane']:
+	print("Hello " + name)
+
+print(" ")
+
+for letter in "abcdefghijklmnopqrstuvwxyz":
+	print(letter.upper())
+
+print(" ")
+
+for num in [1, 2, 3, 4]:
+	if num % 2 == 0:
+		print(num)
+	
+print(" ")
+
+start = 10
+while start:
+	print(start)
+	start -= 1
+
+
+print(" ")
+
+names = ['ana', 'dana', 'ioana', 'QUIT', 'diana']
+for name in names:
+	if name == 'QUIT':
+		break
+	print(name)
+
+
+
+print(" ")
+
+
+for name in names:
+	if name == 'QUIT':
+		continue
+	print(name)
+
+print(" ")
+
