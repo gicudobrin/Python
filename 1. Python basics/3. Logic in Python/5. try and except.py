76c6337291f12266691c1939b0d929661f02b2b4
gicudobrin <mihaiclaudiu87@yahoo.com>
2017-11-04 13:36:29 +0200

    update

---
+++ 1. Python basics/3. Logic in Python/5. try and except.py
@@ -0,0 +1,23 @@
+try:
+	count = int(input("Give me a number: "))
+except ValueError:
+	print("That's not a number!")
+else:
+	print("Hi " * count)
+
+
+
+def add(a,b):
+    try:
+        print("The first number is {}".format(a))
+        print("The second number is {}".format(b))
+        return float(a) + float(b)
+    except ValueError:
+        return None
+    else:
+    	print(a + b)
+        
+
+suma = add(45,25)
+print(suma)
+print("Suma este {}".format(suma))
