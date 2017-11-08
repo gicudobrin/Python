cef3651ad7df5d9e67f5f73b8d1af8bb2352ed19
gicudobrin <mihaiclaudiu87@yahoo.com>
2017-11-08 21:49:40 +0200

    added others updates

--- 2. Python Collections/1. Lists/2. shopping_list_3.py
+++ 2. Python Collections/1. Lists/2. shopping_list_3.py
@@ -1,55 +1,73 @@
-# have a HELP command
-# have a SHOW command
-# clean code up in general
-
 import os
-
-# make a list to hold onto our items
 
 shopping_list = []
 
+
 def clear_screen():
-    os.system("cls" if os.name == "nt" else "clear")
-
-
+  os.system("cls" if os.name == "nt" else "clear")
 
 
 def show_help():
     clear_screen()
-    # print out instructions on how to use the app
-    print("What shall we pick up at the store?")
+    print("What should we pick up at the store?")
     print("""
 Enter 'DONE' to stop adding items.
 Enter 'HELP' for this help.
 Enter 'SHOW' to see your current list.
+Enter 'REMOVE' to delete an item from your list.
 """)
+
+
+def add_to_list(item):
+    show_list()
+    if len(shopping_list):
+        position = raw_input("Where should I add {}?\n"
+                         "Press ENTER to add to the end of the list\n"
+                         "> ".format(item))
+    else:
+        position = 0
+        
+    try:
+        position = abs(int(position))
+    except ValueError:
+        position = None
+    if position is not None:
+        shopping_list.insert(position-1, item)
+    else:
+        shopping_list.append(new_item)
+    
+    show_list()
+
 
 def show_list():
     clear_screen()
-    # print out the list
+    
     print("Here's your list:")
+    
     index = 1
     for item in shopping_list:
-        print("{}. {}".format(index, item))  
-        index += 1 
-    print("-" * 10)
+        print("{}. {}".format(index, item))
+        index += 1
+        
+    print("-"*10)
 
-def add_to_list(new_item):
-     # add new items to our list
-    clear_screen()
-    shopping_list.append(new_item)
-    print("Added {}. List now has {} items.".format(new_item,len(shopping_list)))
 
-show_help()   
+def remove_from_list():
+    show_list()
+    what_to_remove = raw_input("What would you like to remove?\n> ")
+    try:
+        shopping_list.remove(what_to_remove)
+    except ValueError:
+        pass
+    show_list()
+
+
+show_help()
 
 while True:
-    # ask for new items
-    
     new_item = raw_input("> ")
-    
-    # be able to quit the app
-    
-    if new_item.upper() == 'DONE' or new_item.upper() == "QUIT":
+
+    if new_item.upper() == 'DONE' or new_item.upper() == 'QUIT':
         break
     elif new_item.upper() == 'HELP':
         show_help()
@@ -57,7 +75,9 @@ while True:
     elif new_item.upper() == 'SHOW':
         show_list()
         continue
+    elif new_item.upper() == 'REMOVE':
+        remove_from_list()
     else:
         add_to_list(new_item)
-    
+
 show_list()
--- 2. Python Collections/2. Slices/1. Slices.py
+++ 2. Python Collections/2. Slices/1. Slices.py
@@ -1,2 +1,8 @@
 # We don't always want the entirety of a list or string. 
 # Sometimes we just want part of it, and Python calls these sub-string or sub-lists "slices"
+
+# [start:stop]
+
+# Both start and stop are optional. If you leave off start, the slice will start at the beginning of the iterable. If you leave off stop, the slice will continue until the end of the iterable.
+
+# By using this behavior, you can quickly make copies of iterables by slicing them from front to back with [:].
