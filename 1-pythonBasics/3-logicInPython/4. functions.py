580f5c7be7a2ed421efefc8698b97d57568d4477
gicudobrin <mihaiclaudiu87@yahoo.com>
2018-03-12 22:57:23 +0200

    Delete cacat.py

--- 1-pythonBasics/3-logicInPython/4. functions.py
+++ 1-pythonBasics/3-logicInPython/4. functions.py
@@ -23,3 +23,71 @@ print(avg)
 def printer(count):
     print("Hi " * count)
 printer(10)
+
+
+import os
+import random
+
+def clear():
+    if os.name == 'nt':
+    	os.system('cls')
+    else:
+        os.system('clear')
+clear()
+
+prefixes = 'JKLMNOPQ'
+suffix = 'ack'
+index = 0
+for item in prefixes:
+	if prefixes[index] == 'O':
+		print item + "u" + suffix
+	else:
+		print item + suffix
+	index += 1
+
+print "\n"
+
+def count(letter, word):
+	# count = 0
+	# for item in word:
+	# 	if item == letter:
+	# 		count += 1
+	count = word.count(letter)
+	print "The letter {} is in your string for {} times.".format(letter,count)
+
+count("5","55kj5")
+
+def polindrom(word1,word2):
+	if word1[:] == word2[::-1]:
+		# return True
+		print "good"
+	else:
+		print "nope"
+
+polindrom("noot","noon")
+
+
+import os
+#import random
+
+
+def clear():
+	if os.name == 'nt':
+		os.system('cls') 
+	else:
+		os.system('clear')
+
+clear()
+
+numbers = [23,45,34,23,78]
+print numbers
+for i in [0,1,2,3,4]:
+	numbers[i] = numbers[i] * 2
+
+print numbers
+
+
+def greetPerson(*name):
+  print('Hello', name)
+
+greetPerson('Frodo', 'Sauron')
